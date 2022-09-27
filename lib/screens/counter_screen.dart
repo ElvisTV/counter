b07b0b1fbfe7b1735b80a4d9e9a25cbc3e5645f1
Elvis Arturo Tezen Villanueva <elvis.tezen@softtek.com>
2022-09-26 21:07:17 -0500

    First commit

---
+++ lib/screens/counter_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CounterScreeen extends StatefulWidget {
+
+  const CounterScreeen({Key? key}) : super(key: key);
+
+  @override
+  State<CounterScreeen> createState() => _CounterScreeenState();
+}
+
+class _CounterScreeenState extends State<CounterScreeen> {
+  int counter = 10;
+
+  @override
+  Widget build(BuildContext context) {
+
+    const fontSize30 =  TextStyle( fontSize: 30 );
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: const Text ('CounterScreeen'),
+        
+        elevation: 0,
+      ),
+      body: Center (
+         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Número de clicks', style: fontSize30 ),
+            Text( '$counter' , style: fontSize30 )
+          ],          
+         )
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+
+              counter ++;
+              
+              setState(() {
+                
+              });
+            }
+
+          ),
+
+          const SizedBox(width: 20),
+
+           FloatingActionButton(
+            child: const Icon(Icons.ac_unit),
+            onPressed: () {
+
+              counter = 0;
+              
+              setState(() {
+                
+              });
+            }
+
+          ),
+
+           const SizedBox(width: 20),
+
+           FloatingActionButton(
+            child: const Icon(Icons.ac_unit),
+            onPressed: () {
+
+              counter --;
+              
+              setState(() {
+                
+              });
+            }
+
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    const fontSize30 =  TextStyle( fontSize: 30 );
+    int counter = 10;
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: const Text ('HomeScreen'),
+        
+        elevation: 0,
+      ),
+      body: Center (
+         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Número de clicks', style: fontSize30 ),
+            Text( '$counter' , style: fontSize30 )
+          ],          
+         )
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat ,
+
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+
+          counter ++;
+          
+          print('Hola Mundo: $counter' );
+        }
+
+      ),
+    );
+  }
+
+
+}
