736fe67ba7821344afc2e59a3633ad0281d89130
Elvis Arturo Tezen Villanueva <elvis.tezen@softtek.com>
2022-09-26 21:09:52 -0500

    cambio uno

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CounterScreeen extends StatefulWidget {
+  
 
   const CounterScreeen({Key? key}) : super(key: key);
 
